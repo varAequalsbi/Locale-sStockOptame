f30f2ffe6e7e0c3517b78ea45e3b87605653e7e4
Varabi Mawardi <alfarabimam3@gmail.com>
2026-01-08 18:07:03 +0700

    wopsie

--- src/firebase.js
+++ src/firebase.js
@@ -4,24 +4,13 @@ import { getFirestore } from "firebase/firestore";
 
 // We use Environment Variables so your keys are not public on GitHub
 const firebaseConfig = {
-
-  apiKey: "AIzaSyBJQuONWd3yROCM5e9_LtChSwVWPAKTDnw",
-
-  authDomain: "stockoptamelocale.firebaseapp.com",
-
-  projectId: "stockoptamelocale",
-
-  storageBucket: "stockoptamelocale.firebasestorage.app",
-
-  messagingSenderId: "536081509072",
-
-  appId: "1:536081509072:web:26570bff254ab22ce90cd5",
-
-  measurementId: "G-S4G1N7CC9W"
-
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
-
-
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
