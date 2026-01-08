0eeedd750f1acf02cebfa1729db26d7bb3a002f5
Varabi Mawardi <alfarabimam3@gmail.com>
2026-01-08 18:50:56 +0700

    o

--- src/firebase.js
+++ src/firebase.js
@@ -10,6 +10,6 @@ const firebaseConfig = {
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
   measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
-
+console.log("Firebase Project ID:", import.meta.env.VITE_FIREBASE_PROJECT_ID);
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
